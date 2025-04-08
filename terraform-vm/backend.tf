0bf8ad35fa8b6c0cb8ec7e9ff02ebf9123bd404d
DragneelAyan <ayandragneelsinha@gmail.com>
2025-04-08 21:35:24 +0530

    commit

--- terraform-vm/backend.tf
+++ terraform-vm/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "terraform-backend-rg"
-    storage_account_name = "terraformbackend123"
+    resource_group_name  = "terraform-backend-rg69"
+    storage_account_name = "terraformbackend69"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
