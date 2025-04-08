80a0b84cba8e8e87a04ed622673f967cfde1674e
DragneelAyan <ayandragneelsinha@gmail.com>
2025-04-08 21:30:42 +0530

    Powershell changes.

--- powershell/storageAcc.ps1
+++ powershell/storageAcc.ps1
@@ -1,8 +1,8 @@
 # Variables
-resourceGroupName="terraform-backend-rg69"
-location="centralindia"
-storageAccountName="terraformbackend69"   # must be globally unique
-containerName="tfstate"
+$resourceGroupName="terraform-backend-rg69"
+$location="centralindia"
+$storageAccountName="terraformbackend69"   # must be globally unique
+$containerName="tfstate"
 
 # Create resource group
 az group create --name $resourceGroupName --location $location
@@ -11,7 +11,7 @@ az group create --name $resourceGroupName --location $location
 az storage account create --name $storageAccountName --resource-group $resourceGroupName --location $location --sku Standard_LRS
 
 # Get storage account key
-accountKey=$(az storage account keys list --resource-group $resourceGroupName --account-name $storageAccountName --query '[0].value' -o tsv)
+$accountKey = (az storage account keys list --resource-group $resourceGroupName --account-name $storageAccountName --query '[0].value' -o tsv)
 
-# Create container
+# Create blob container
 az storage container create --name $containerName --account-name $storageAccountName --account-key $accountKey
